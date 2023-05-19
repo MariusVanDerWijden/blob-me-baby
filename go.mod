469f11b12b0d25a4f43a2b74ad0a4d2c3155c048
Marius van der Wijden <m.vanderwijden@live.de>
2023-05-19 11:12:09 +0200

    initial commit: basic blob creation app

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/MariusVanDerWijden/blob-me-baby
+
+go 1.20
+
+require github.com/ethereum/go-ethereum v1.11.6
+
+require (
+	github.com/bits-and-blooms/bitset v1.7.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.11.0 // indirect
+	github.com/crate-crypto/go-kzg-4844 v0.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/holiman/uint256 v1.2.2-0.20230321075855-87b91420868c // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/protolambda/ztyp v0.2.1 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
+)
+
+replace github.com/ethereum/go-ethereum => github.com/mdehoog/go-ethereum v1.10.19-0.20230509130245-a0cb9316a9c8
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Starting server at port 80")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/random", handleRandom).
+		Methods("GET")
+	router.HandleFunc("/encode/{data}", handleEncode).
+		Methods("GET")
+	if err := http.ListenAndServe(":80", router); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleRandom(rw http.ResponseWriter, req *http.Request) {
+	// TODO impl
+}
+
+type CommitmentAndProof struct {
+	Commitments     []types.KZGCommitment
+	VersionedHashes []common.Hash
+	AggregatedProof []types.KZGProof
+}
+
+func handleEncode(rw http.ResponseWriter, req *http.Request) {
+	data := common.FromHex(mux.Vars(req)["data"])
+	blobs := encodeBlobs(data)
+	commits, versionedHashes, aggProof, err := blobs.ComputeCommitmentsAndProofs()
+	if err != nil {
+		fmt.Printf("Error computing commitment: %v\n", err)
+		rw.WriteHeader(500)
+		return
+	}
+
+	result := CommitmentAndProof{
+		Commitments:     commits,
+		VersionedHashes: versionedHashes,
+		AggregatedProof: aggProof,
+	}
+
+	resp, err := json.Marshal(result)
+	if err != nil {
+		fmt.Printf("Error marshalling result: %v\n", err)
+		rw.WriteHeader(500)
+		return
+	}
+	rw.Write(resp)
+}
+
+func encodeBlobs(data []byte) types.Blobs {
+	blobs := []types.Blob{{}}
+	blobIndex := 0
+	fieldIndex := -1
+	for i := 0; i < len(data); i += 31 {
+		fieldIndex++
+		if fieldIndex == params.FieldElementsPerBlob {
+			blobs = append(blobs, types.Blob{})
+			blobIndex++
+			fieldIndex = 0
+		}
+		max := i + 31
+		if max > len(data) {
+			max = len(data)
+		}
+		copy(blobs[blobIndex][fieldIndex*32:], data[i:max])
+	}
+	return blobs
+}
